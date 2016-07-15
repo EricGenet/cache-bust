e47a561315a844b4ae34e21d53a9265812b1a6cf
Mihai Rotaru <mihai.rotaru@gmx.com>
2016-07-15 07:57:12 +0100

    parse options, resolve globs, relative paths

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 'use strict'
 
+let glob = require('glob')
+let args = require('minimist')(process.argv.slice(2))
+let fs = require('fs')
+let path = require('path')
+let merge = require('deep-extend')
+
+console.log(args)
+
+let options = {
+  baseDir: args.hasOwnProperty('baseDir')
+    ? args.baseDir
+    : __dirname
+}
+
+let files = args['_']
+  .reduce((files, pattern) => files.concat(
+    glob.sync(pattern).map((f) => path.relative(options.baseDir, f))
+  ), [])
+
+console.dir(files)
+
 /**
  * - glob for files
  * - is cwd ? compose relative paths
