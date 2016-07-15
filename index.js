f3b5f5f5fe0179650dc4b5956b1dc453e3f94aa9
Mihai Rotaru <mihai.rotaru@gmx.com>
2016-07-15 23:59:18 +0100

    hashing and renaming

--- index.js
+++ index.js
@@ -5,27 +5,68 @@ let args = require('minimist')(process.argv.slice(2))
 let fs = require('fs')
 let path = require('path')
 let merge = require('deep-extend')
+let crypto = require('crypto')
 
-console.log(args)
-
+/**
+ * Parse command line arguments, set defaults
+ */
 let options = {
-  baseDir: args.hasOwnProperty('baseDir')
-    ? args.baseDir
-    : __dirname
+  baseDirExisting: args.hasOwnProperty('baseDirExisting')
+    ? args.baseDirExisting
+    : __dirname,
+  baseDirNew: args.hasOwnProperty('baseDirNew')
+    ? args.baseDirNew
+    : __dirname,
+  hexLength: args.hasOwnProperty('hexLength')
+    ? args.hexLength
+    : 7
+}
+if (!args['referenced'] || !args['referencing']) {
+  console.error('`referenced` and `referencing` options must be specified')
+  process.exit(1)
 }
 
-let files = args['_']
+/**
+ * Allow multiple glob patterns, separated by commas or spaces
+ */
+let filesFn = (str) => str.split(/[, ]/)
   .reduce((files, pattern) => files.concat(
-    glob.sync(pattern)
-      .map((f) => path.relative(options.baseDir, f))
-  ), [])
+    glob.sync(pattern)), [])
 
-console.dir(files)
+/**
+ * Generate an array to store each referenced file's absolute path, relative
+ * path (from `baseDir`) and the MD5 hash
+ */
+let referenced = filesFn(args['referenced'])
+  .map((f) => {
+    return {
+      abs: path.resolve(f),
+      relativeExisting: path.relative(options.baseDirExisting, f),
+      relativeNew: path.relative(options.baseDirNew, f),
+      hash: crypto.createHash('md5').update(fs.readFileSync(path.resolve(f))).digest('hex')
+    }
+  })
+  .filter((f) => fs.lstatSync(f.abs).isFile())
 
-for (let f of files) {
-  // - hash
-  // - copy
-  // - replace in target files
+/**
+ * move/rename (potentially) referenced files
+ */
+let newName = (p, hash) => {
+  let ext = path.extname(p)
+  let base = path.basename(p, ext)
+  let hashPart = hash.substring(0, options.hexLength)
+  return `${base}-${hashPart}${ext}`
+}
+for (let f of referenced) {
+  fs.renameSync(f.abs, path.join(path.dirname(f.abs), newName(f.abs, f.hash)))
+}
+
+/**
+ * Go over each file containing references to `referencedFiles` and perform the
+ * replacements
+ */
+for (let f of filesFn(args['referencing'])) {
+  // replace occurences
 }
 
 let cacheBust = {
