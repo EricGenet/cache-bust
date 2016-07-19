e3896337346db4f8451470de85219b33f9cb4681
Mihai Rotaru <mihai.rotaru@gmx.com>
2016-07-19 21:43:41 +0100

    refctor: mv cli into own file

---
+++ cli.js
@@ -0,0 +1,28 @@
+let args = require('minimist')(process.argv.slice(2))
+let cacheBust = require('./index.js')
+
+/**
+ * Parse command line arguments, set defaults
+ */
+if (!args['referenced'] || !args['referencing']) {
+  console.error('`referenced` and `referencing` options must be specified')
+  process.exit(1)
+}
+let getOpt = (long, short, defaultValue) => {
+  if (args.hasOwnProperty(long)) {
+    return args[long]
+  } else if (args.hasOwnProperty(short)) {
+    return args[short]
+  }
+  return defaultValue
+}
+
+let options = {
+  referenced: getOpt('referenced', 'r', ''),
+  referencing: getOpt('referencing', 'R', ''),
+  baseDir: getOpt('baseDir', 'b', __dirname),
+  hexLength: getOpt('hexLength', 'l', 7),
+  exclude: getOpt('exclude', 'e', [])
+}
+
+cacheBust(options)
