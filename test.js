0b52e651761a66b60e9d9687e43b15678ddb7827
Mihai Rotaru <mihai.rotaru@gmx.com>
2016-07-19 21:44:20 +0100

    testing

--- test.js
+++ test.js
@@ -1,29 +1,63 @@
 'use strict'
 
-let mock = require('mock-fs')
+let fsMock = require('mock-fs')
 let assert = require('assert')
 let fs = require('fs')
+let merge = require('deep-extend')
 
 let cacheBust = require('./index.js')
 
-// a file is hashed with MD5, and the occurences in another file are replaced
-mock({
-  'index.html': '<img src="img/foo.png">',
+let testFileStructure = {
   'img': {
     'foo.png': 'FAKE_PNG_DATA'
+  },
+  'index.html': '<img src="img/foo.png">'
+}
+
+let testOptions = {
+  baseDir: __dirname,
+  referenced: 'img/*.png',
+  referencing: '*.html',
+  exclude: [],
+  hexLength: 7
+}
+
+// basic
+fsMock(testFileStructure)
+
+cacheBust(testOptions)
+
+assert(fs.existsSync('img/foo-ac3e140.png'))
+assert(fs.readFileSync('index.html', 'utf8') === '<img src="img/foo-ac3e140.png">')
+
+fsMock.restore()
+
+// ignore/exclude
+fsMock(merge({}, testFileStructure, {
+  'img': {
+    'bar.jpg': 'FAKE_JPG_DATA'
   }
-})
+}))
 
-cacheBust.bust({
-  assets: 'img/*',
-  files: '**/*.html'
-})
+cacheBust(merge({}, testOptions, {
+  exclude: ['img/*.jpg']
+}))
 
-assert(fs.existSync('img/foo-ac3e140.png'))
-assert(fs.readFileSync('index.html') === '<img src="img/foo-ac3e140.png">')
+assert(fs.existsSync('img/foo-ac3e140.png'))
+assert(fs.readFileSync('index.html', 'utf8') === '<img src="img/foo-ac3e140.png">')
 
-// check old assets were removed
+fsMock.restore()
 
-// check replacements
-
-// cwd - composing relative paths
+// multiple files
+fsMock(merge({}, testFileStructure, {
+  'img': {
+    'bar.jpg': 'FAKE_JPG_DATA' // 4a90024c4cbc7a9447fb3652852bc832
+  },
+  'index.html': '<img src="img/foo.png"><img src="img/bar.jpg">'
+}))
+cacheBust(merge({}, testOptions, {
+  referenced: 'img/*.{png,jpg}'
+}))
+assert(fs.existsSync('img/foo-ac3e140.png'))
+assert(fs.existsSync('img/bar-4a90024.jpg'))
+assert(fs.readFileSync('index.html', 'utf8') === '<img src="img/foo-ac3e140.png"><img src="img/bar-4a90024.jpg">')
